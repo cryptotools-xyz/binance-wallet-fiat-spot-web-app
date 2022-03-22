9577adb1f0f109a80bb1ce3215fbe8782f1d1453
Souhail M <souhail.merroun1996@gmail.com>
2022-03-22 14:03:49 +0100

    Setup password before api call

--- src/App.tsx
+++ src/App.tsx
@@ -4,36 +4,40 @@ import 'bootstrap/dist/css/bootstrap.css';
 import ReactTable from './components/ReactTable';
 
 function App() {
-  useEffect(() => {
+  const [password, setPassword] = useState("");
+  const [balances, setBalances] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleClick = async () => {
     const url = process.env.REACT_APP_WEB_API_URL
-    const password = process.env.REACT_APP_WEB_API_PASSWORD
 
     if (!url) {
       throw new Error("The REACT_APP_WEB_API_URL variable is not defined.");
     }
-
+    
     setLoading(true);
-    const fetchBalances = async () => {
-      try {
-        const response = await fetch(url + "/api/wallet/fiat-spot?password=" + password);
-        const { balances } = await response.json();
-        setBalances(balances);
-        setLoading(false);
-      } catch (error) {
-        console.log("error", error);
-        setLoading(false);
-      }
-    };
+    try {
+      const response = await fetch(url + "/api/wallet/fiat-spot?password=" + password);
+      const { balances } = await response.json();
+      setBalances(balances);
+      setLoading(false);
+    } catch (error) {
+      console.log("error", error);
+      setLoading(false);
+      setError(true);
+    }
+  }
 
-    fetchBalances();
-  }, []);
-
-  const [balances, setBalances] = useState([]);
-  const [loading, setLoading] = useState(false);
+  console.log("loading", loading)
+  console.log("error", error)
+  console.log("balances", balances)
 
   return (
     <div>
       <h1>Binance wallet fiat & spot</h1>
+      <input value={password} onChange={e => setPassword(e.target.value)} type="password" />
+      <button onClick={handleClick}>Get data</button>
 
       {loading ? <p>We're loading, please wait.</p> : <>
         <ReactTable data={balances} />
